4cfbf04a775021e24c7c055b95784c66cc3c5ab1
belotti-nicola <belotti.nicola@outlook.it>
2022-04-23 09:22:37 +0200

    simple commit to ignore

--- src/databases/logic_functions/xor_db.py
+++ src/databases/logic_functions/xor_db.py
@@ -1,11 +1,13 @@
 from src.utils.csv import readCSV
 from src.databases.abstract_database import absDatabase
 
+from random import shuffle
+
 class Xor(absDatabase):
     def __init__(self) -> None:
         self.test  = readCSV('src/databases/logic_functions/test.csv')
         self.train = readCSV('src/databases/logic_functions/train.csv')
-
+        self.test = random.shuffle()
 
     def getBatchTest(self, dim):
         pass
