5eb6ad996aef0e4765ab56c9b3d42eaff6845384
belotti-nicola <belotti.nicola@outlook.it>
2022-04-25 18:27:42 +0200

    im back

--- src/databases/logic_functions/xor_db.py
+++ src/databases/logic_functions/xor_db.py
@@ -1,20 +1,25 @@
 from src.utils.csv import readCSV
 from src.databases.abstract_database import absDatabase
 
-from random import shuffle
+import random
 
 class Xor(absDatabase):
     def __init__(self) -> None:
         self.test  = readCSV('src/databases/logic_functions/test.csv')
-        self.train = readCSV('src/databases/logic_functions/train.csv')
         self.test = random.shuffle()
+        self.ntest = len(self.train)
+
+        self.train = readCSV('src/databases/logic_functions/train.csv')
+        self.train = random.shuffle()
+        self.ntrain = len(self.train)
 
     def getBatchTest(self, dim):
-        pass
-    def getBatchTest(self, dim):
-        pass
+        if dim < self.ntest:
+            tmp = random.shuffle()
+            return tmp[0:dim]
+        
     def getDatabaseTest(self):
-        pass
+        return self.test
     def getDatabaseTrain(self):
-        pass
+        return self.train
     
