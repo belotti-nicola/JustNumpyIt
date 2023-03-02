0478057ec493dc77a4936b1197a50e663bb83f10
belotti-nicola <belotti.nicola@outlook.it>
2023-03-02 23:29:24 +0100

    added GD

--- src/algorithms/gradient_descent.py
+++ src/algorithms/gradient_descent.py
@@ -1,2 +1,10 @@
-def GD():
+def estimate_gradient(f,d):
     pass
+
+def GD(x0,direction,alpha,ITERATIONS):
+    i=0
+    x_i = x0
+    while i < ITERATIONS:
+        direction = estimate_gradient(fun,data)
+        x_i = x0+alpha*direction
+    return x_i
