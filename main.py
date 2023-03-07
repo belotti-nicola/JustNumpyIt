107aa4f3f607838372c6def34f7e6bbaae9a1f0e
belotti-nicola <belotti.nicola@outlook.it>
2023-03-07 21:55:14 +0100

    more and more entertaining

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from src.utils.math_functions.softmax import SoftMax
 import numpy as np
 
 ITERATIONS = 1
+alpha = .2
 
 data        = IDX('data/MNIST/train-images.idx3-ubyte').numpy().reshape(60000,784).T
 labels      = IDX('data/MNIST/train-labels.idx1-ubyte').numpy()
@@ -32,21 +33,37 @@ def forward_propagation(data,A1,b1,A2,b2):
     y2 = SoftMax.fun(np.dot(A2,y1) + b2)
     return y1,y2
 
-def backward_propagation(a,b):
-    pass
+def backward_propagation(y1,y2):
+    print(y2.shape)
+    dA2 = np.dot(SoftMax.der(y2),y2)
+    db2 = np.dot(SoftMax.der(y2),1)
+    dA1 = np.dot(
+            dA2,
+            np.dot(ReLU.der(y1),y1)
+    )
+    db1 = np.dot(
+            dA2,
+            np.dot(ReLU.der(y1),1)
+    )
+
+    return dA1,db1,dA2,db2
+
 
 def label_encoding(vector): 
     dim = max(vector.shape)
-    substitution_for_vector = np.zeros(vector.shape,dtype=int)
+    substitution_for_vector = np.zeros((dim,10),dtype=int)
     for i in range(dim):
         index = vector[i]
         substitution_for_vector[i][index] = 1
     return substitution_for_vector
 
 
-labels_matrix = label_encoding(labels)
+labels_matrix = label_encoding(labels).T
 for i in range(ITERATIONS):
     y1,y2 = forward_propagation(data,A1,b1,A2,b2)
-    LOSS = y2 - labels_matrix
-    #backward_propagation(y1,y2)
- 
+    LOSS = ((y2 - labels_matrix)**2).mean(axis=1)
+    dA1,db1,dA2,db2 = backward_propagation(y1,y2)
+    A1 = A1 + alpha * dA1 
+    b1 = b1 + alpha * db1 
+    A2 = A2 + alpha * dA2
+    b2 = b2 + alpha * db2
